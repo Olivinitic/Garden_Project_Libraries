b868bb7d7dab56a7fbfa7b684711f9ab9ea2b858
Olivinitic <osalmeron32@gmail.com>
2016-06-21 21:28:11 -0700

    Initial Libraries Commit
    This is the First set of libraries that are needed for the Garden
    Monitor Project.

---
+++ Adafruit_FeatherOLED/examples/tester/tester.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_FeatherOLED.h>
+
+Adafruit_FeatherOLED oled = Adafruit_FeatherOLED();
+
+void setup()
+{
+  oled.init();
+  oled.refresh();
+}
+
+void loop()
+{
+
+}
